Fixed scaling bugs in infogan

cytogan/metrics/losses.py
@@ -1,12 +1,21 @@
 import keras.backend as K
 import numpy as np
-import tensorflow as tf
 
 E = 1e-10  # numerical stability
 
 
-def binary_crossentropy(p, q):
+def cross_entropy(p, q):
     '''H(p, q) = -sum_i(p_i * log(q_i))'''
+    with K.name_scope('crossentropy'):
+        return -K.mean(p * K.log(E + q))
+
+
+def entropy(p):
+    '''H(p) = -sum_i(p_i * log(p_i))'''
+    return cross_entropy(p, p)
+
+
+def binary_crossentropy(p, q):
     with K.name_scope('binary_crossentropy'):
         pointwise = p * K.log(E + q) + (1 - p) * K.log(E + 1 - q)
         value = -K.mean(pointwise)
@@ -30,13 +39,11 @@ def reconstruction_loss(original_images, reconstructed_images):
 def mutual_information(x, x_given_y):
     '''I(x;y) = H(x) - H(x|y)'''
     with K.name_scope('mutual_information'):
-        with tf.control_dependencies([tf.assert_positive(x)]):
-            # The cross entropy between x and x is just the entropy H(x).
-            h_x = K.categorical_crossentropy(x, x)
-            # The cross entropy between x and x|y is H(x|y).
-            h_x_given_y = K.categorical_crossentropy(x, x_given_y)
-            # The mutual information I(x;y) is now H(x) - H(x|y). Usually we
-            # want to maximize mutual information, but to provide a minimizable
-            # objective for TF's optimizer, we return E[-(H(x) - H(x|y))] =
-            # E[H(x|y) - H(x).]
+        h_x = entropy(x)
+        # The cross entropy between x and x|y is H(x|y).
+        h_x_given_y = cross_entropy(x, x_given_y)
+        # The mutual information I(x;y) is now H(x) - H(x|y).
+        # Usually we want to maximize mutual information, but to provide a
+        # minimizable objective for TF's optimizer, we return E[-(H(x) -
+        # H(x|y))] = E[H(x|y) - H(x).]
         return K.mean(h_x_given_y - h_x)

cytogan/models/infogan.py
@@ -56,7 +56,7 @@ class InfoGAN(model.Model):
         self.encoder = None  # Q(c|x)
         self.encoder_loss = None
 
-        super(InfoGAN, self).__init__([learning], session)
+        super(InfoGAN, self).__init__(learning, session)
 
     def _define_graph(self):
         tensors = self._define_generator()
@@ -112,7 +112,7 @@ class InfoGAN(model.Model):
         else:
             number_of_samples = len(latent_samples)
         noise = self._sample_noise(number_of_samples)
-        return self.session.run(
+        images = self.session.run(
             self.generator.output,
             feed_dict={
                 self.latent_prior: latent_samples,
@@ -120,7 +120,11 @@ class InfoGAN(model.Model):
                 K.learning_phase(): 0,
             })
 
+        # Go from [-1, +1] scale back to [0, 1]
+        return (images + 1) / 2
+
     def train_on_batch(self, real_images, with_summary=False):
+        real_images = (real_images * 2) - 1
         discriminator_loss = self._train_discriminator(real_images)
 
         noise = self._sample_noise(len(real_images))
@@ -184,7 +188,7 @@ class InfoGAN(model.Model):
                 D = LeakyReLU(alpha=0.2)(D)
             D = Flatten()(D)
 
-        with tf.control_dependencies([tf.assert_positive(x)]):
+        with tf.control_dependencies([tf.assert_positive(D, [D])]):
             return x, D
 
     def _train_discriminator(self, real_images):

cytogan/models/model.py
@@ -16,6 +16,8 @@ Learning = collections.namedtuple('Learning',
 
 class Model(abc.ABC):
     def __init__(self, learning, session):
+        assert isinstance(learning, Learning)
+
         self.session = session
 
         # The training step indicator variable.
@@ -84,18 +86,14 @@ class Model(abc.ABC):
                     checkpoint))
         self.saver.restore(self.session, checkpoint)
 
-    def _add_optimizer(self, learning_options, losses):
+    def _add_optimizer(self, learning, losses):
         learning_rate, optimizer = {}, {}
         return_tensors = False
         if isinstance(losses, tf.Tensor):
             return_tensors = True
             losses = {0: losses}
-            learning_options = [learning_options] * len(losses)
-        if len(learning_options) < len(losses):
-            learning_options = [learning_options[0]] * len(losses)
-        assert len(losses) == len(learning_options)
-        for learning, key in zip(learning_options, losses):
-            lr = self._get_learning_rate(learning)
+        for index, key in enumerate(losses.keys()):
+            lr = self._get_learning_rate(learning, index)
             kwargs = learning.kwargs or {}
             loss = tf.check_numerics(losses[key], str(key))
             optimizer[key] = tf.train.AdamOptimizer(lr, **kwargs).minimize(
@@ -106,14 +104,18 @@ class Model(abc.ABC):
             return learning_rate[0], optimizer[0]
         return learning_rate, optimizer
 
-    def _get_learning_rate(self, learning):
-        # Start with the scalar learning rate value.
+    def _get_learning_rate(self, learning, index):
+        learning_rate = learning.rate
+        if isinstance(learning_rate, collections.Iterable):
+            learning_rate = learning_rate[index]
         if learning.decay is None:
-            return learning.rate
-        # Upgrade to decaying learning rate *tensor*.
+            return learning_rate
+        decay = learning.decay
+        if isinstance(decay, collections.Iterable):
+            decay = decay[index]
         return tf.train.exponential_decay(
-            learning.rate,
+            learning_rate,
             decay_steps=learning.steps_per_decay,
-            decay_rate=learning.decay,
+            decay_rate=decay,
             global_step=self.global_step,
             staircase=True)

cytogan/train/trainer.py
@@ -97,8 +97,11 @@ class Trainer(object):
 
     def _update_progressbar(self, batch_range, learning_rate, loss):
         if isinstance(loss, collections.Mapping):
-            assert all(np.isscalar(l) for l in loss.values())
-            batch_range.set_postfix(**loss, lr=learning_rate)
+            strings = {}
+            for key, value in loss.items():
+                assert np.isscalar(value), key
+                strings[key]= '{0:.8f}'.format(value)
+            batch_range.set_postfix(**strings, lr=learning_rate)
         else:
             assert np.isscalar(loss)
             batch_range.set_postfix(loss=loss, lr=learning_rate)