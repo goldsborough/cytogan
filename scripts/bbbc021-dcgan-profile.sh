a07312eddce491817286bca915ccf04cead81ee9
goldsborough <peter@goldsborough.me>
2017-09-18 16:08:22 -0400

    Update launch scripts

--- scripts/bbbc021-dcgan-profile.sh
+++ scripts/bbbc021-dcgan-profile.sh
@@ -3,23 +3,22 @@
 rm dcgan.prof
 
 python3 -m cProfile -o dcgan.prof cytogan/train/bbbc021.py \
-  --epochs 20                                             \
-  --model dcgan                                           \
-  --lr 5e-4 2e-4                                          \
-  --lr-decay 0.9975                                       \
-  --lr-decay-steps 100                                    \
-  --batch-size 64                                         \
-  --workspace /data1/peter/runs                           \
-  --checkpoint-freq '1min'                                \
-  --summary-freq '1min'                                   \
-  --latent-samples 256                                    \
-  --generative-samples 5                                  \
-  --gpus 2                                                \
-  --metadata /data1/peter/metadata/BBBC021_v1_image.csv   \
-  --labels /data1/peter/metadata/BBBC021_v1_moa.csv       \
-  --images /data1/peter/segmented                         \
-  --cell-count-file /data1/peter/metadata/cell_counts.csv \
-  --latent-compounds                                      \
-  --latent-moa                                            \
-  --confusion-matrix                                      \
+  --epochs 30                                              \
+  --model dcgan                                            \
+  --lr 5e-4 2e-4                                           \
+  --lr-decay 0.999                                         \
+  --lr-decay-steps 100                                     \
+  --batch-size 128                                         \
+  --workspace /data1/peter/runs                            \
+  --checkpoint-freq '1min'                                 \
+  --summary-freq '1min'                                    \
+  --latent-samples 256                                     \
+  --generative-samples 5                                   \
+  --gpus 1 2                                               \
+  --metadata /data1/peter/metadata/BBBC021_v1_image.csv    \
+  --labels /data1/peter/metadata/BBBC021_v1_moa.csv        \
+  --images /data1/peter/segmented                          \
+  --latent-compounds                                       \
+  --latent-moa                                             \
+  --confusion-matrix                                       \
   -p 'Week3_25421'
--- scripts/bbbc021-dcgan.sh
+++ scripts/bbbc021-dcgan.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 python3 -m cytogan.train.bbbc021                          \
-  --epochs 20                                             \
+  --epochs 30                                             \
   --model dcgan                                           \
   --lr 5e-4 2e-4                                          \
-  --lr-decay 0.9975                                       \
+  --lr-decay 0.999                                        \
   --lr-decay-steps 100                                    \
-  --batch-size 256                                        \
+  --batch-size 128                                        \
   --workspace /data1/peter/runs                           \
   --checkpoint-freq '1min'                                \
   --summary-freq '1min'                                   \
   --latent-samples 256                                    \
   --generative-samples 5                                  \
-  --gpus 2 3                                              \
+  --gpus 1 2                                              \
   --metadata /data1/peter/metadata/BBBC021_v1_image.csv   \
   --labels /data1/peter/metadata/BBBC021_v1_moa.csv       \
   --images /data1/peter/segmented                         \
