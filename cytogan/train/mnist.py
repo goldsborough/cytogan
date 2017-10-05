88137ecf7226ee7fbfaae2a5ce8cc556f168a0a9
goldsborough <peter@goldsborough.me>
2017-10-05 11:46:09 -0400

    Trying different infogan for mnist

--- cytogan/train/mnist.py
+++ cytogan/train/mnist.py
@@ -88,9 +88,9 @@ elif options.model == 'infogan':
     })
     hyper = infogan.Hyper(
         image_shape,
-        generator_filters=(128, 64, 32, 16),
+        generator_filters=(256, 128, 64, 32),
         generator_strides=(1, 2, 2, 1),
-        discriminator_filters=(16, 32, 64, 128),
+        discriminator_filters=(32, 64, 128, 256),
         discriminator_strides=(1, 2, 2, 2),
         latent_size=12,
         noise_size=100,
