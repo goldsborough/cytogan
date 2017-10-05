6c4703b64490a5fb85f7f99cee3c1ed4b3b3ad1c
goldsborough <peter@goldsborough.me>
2017-10-05 12:08:16 -0400

    Fix setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,18 +11,6 @@ sudo apt-get install -y \
 
 sudo apt-get install -y emacs vim
 
-# Create a system wide NVBLAS config
-# See http://docs.nvidia.com/cuda/nvblas/
-NVBLAS_CONFIG_FILE=/etc/nvidia/nvblas.conf
-cat << EOF >> ${NVBLAS_CONFIG_FILE}
-# Insert here the CPU BLAS fallback library of your choice.
-# The standard libblas.so.3 defaults to OpenBLAS, which does not have the
-# requisite CBLAS API.
-NVBLAS_CPU_BLAS_LIB /usr/lib/libblas/libblas.so
-
-# Use all GPUs
-NVBLAS_GPU_LIST ALL
-
 pip3 install --upgrade pip && pip3 install tensorflow-gpu ipython
 
 git clone https://github.com/goldsborough/cytogan
