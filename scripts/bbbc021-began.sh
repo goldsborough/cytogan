a46236070667979b24a04d465bc3fb8b1a574f30
goldsborough <peter@goldsborough.me>
2017-10-05 19:14:26 -0400

    Updated bbbc021-began launch script

--- scripts/bbbc021-began.sh
+++ scripts/bbbc021-began.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 python3 -m cytogan.train.bbbc021                          \
-  --epochs 100                                            \
+  --epochs 30                                             \
   --model began                                           \
   --lr 1e-5 1e-5                                          \
-  --lr-decay 0.9995                                       \
-  --lr-decay-steps 100                                    \
+  --lr-decay 0.9999                                       \
+  --lr-decay-steps 1000                                   \
   --batch-size 16                                         \
   --workspace /data1/peter/runs                           \
   --checkpoint-freq '30s'                                 \
   --summary-freq '30s'                                    \
   --latent-samples 256                                    \
-  --generative-samples 50                                 \
-  --gpus 1 2                                              \
+  --generative-samples 10                                 \
+  --gpus 0 1 2 3                                          \
   --metadata /data1/peter/metadata/BBBC021_v1_image.csv   \
   --labels /data1/peter/metadata/BBBC021_v1_moa.csv       \
   --images /data1/peter/segmented                         \
