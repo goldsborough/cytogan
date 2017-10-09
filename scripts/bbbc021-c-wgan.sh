1e83689a4609a63975b2176a1b668508cd720b8c
goldsborough <peter@goldsborough.me>
2017-10-09 16:09:54 -0400

    Adding c-wgan launch script

--- scripts/bbbc021-c-wgan.sh
+++ scripts/bbbc021-c-wgan.sh
@@ -2,16 +2,16 @@
 
 python3 -m cytogan.train.bbbc021                          \
   --epochs 50                                             \
-  --model wgan                                            \
+  --model c-wgan                                          \
   --lr 8e-5 8e-5                                          \
   --lr-decay 0.9995                                       \
   --lr-decay-steps 100                                    \
-  --batch-size 64                                         \
+  --batch-size 128                                        \
   --workspace /data1/peter/runs                           \
   --checkpoint-freq '10min'                               \
   --summary-freq '10min'                                  \
   --latent-samples 256                                    \
-  --generative-samples 100                                \
+  --generative-samples 100                                 \
   --gpus 4 5 6 7                                          \
   --metadata /data1/peter/metadata/BBBC021_v1_image.csv   \
   --labels /data1/peter/metadata/BBBC021_v1_moa.csv       \
