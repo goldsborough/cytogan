56945ec5db801db032d6cc70b923ec034f14b9a9
goldsborough <peter@goldsborough.me>
2017-10-09 15:34:42 -0400

    Update infogan launch script

--- scripts/bbbc021-infogan.sh
+++ scripts/bbbc021-infogan.sh
@@ -3,8 +3,8 @@
 python3 -m cytogan.train.bbbc021                          \
   --epochs 50                                             \
   --model infogan                                         \
-  --lr 1e-5 8e-5 8e-5                                     \
-  --lr-decay 0.999                                        \
+  --lr 7e-5 2e-4 2e-4                                     \
+  --lr-decay 0.9992                                       \
   --lr-decay-steps 100                                    \
   --batch-size 128                                        \
   --workspace /data1/peter/runs                           \
@@ -12,7 +12,7 @@ python3 -m cytogan.train.bbbc021                          \
   --summary-freq '10min'                                  \
   --latent-samples 100                                    \
   --generative-samples 100                                \
-  --gpus 0 1                                              \
+  --gpus 2 3                                              \
   --metadata /data1/peter/metadata/BBBC021_v1_image.csv   \
   --labels /data1/peter/metadata/BBBC021_v1_moa.csv       \
   --images /data1/peter/segmented                         \
