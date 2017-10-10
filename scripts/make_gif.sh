a11c6e7d7a21e2a38c10ba86d4eab6c9275a4176
goldsborough <peter@goldsborough.me>
2017-10-10 19:39:01 -0400

    Fixed gif script

--- scripts/make_gif.sh
+++ scripts/make_gif.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-convert -background white -alpha remove -layers OptimizePlus -delay 20 \
-  $1/*.png -loop 0 ${2:-animated.gif}
+convert -background white -alpha remove -layers OptimizePlus -delay 10 \
+  "$1/%d.png[0-$(($(ls $1 | wc -l)-1))]" -loop 0 ${2:-animated.gif}
