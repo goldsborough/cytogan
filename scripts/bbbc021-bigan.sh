aaf178d8419d6d0f3c1e26e76c8a49ca481d67cc
goldsborough <peter@goldsborough.me>
2017-10-20 23:30:42 -0400

    Try smaller batch size to avoid OOM

--- scripts/bbbc021-bigan.sh
+++ scripts/bbbc021-bigan.sh
@@ -6,12 +6,12 @@ python3 -m cytogan.train.bbbc021                          \
   --lr 1e-4 1e-4 1e-4                                     \
   --lr-decay 0.9999                                       \
   --lr-decay-steps 100                                    \
-  --batch-size 128                                        \
+  --batch-size 64                                         \
   --workspace /data1/peter/runs                           \
   --checkpoint-freq '10min'                               \
   --summary-freq '10min'                                  \
   --generative-samples 196                                \
-  --gpus 1 2                                              \
+  --gpus 2 3                                              \
   --metadata /data1/peter/metadata/BBBC021_v1_image.csv   \
   --labels /data1/peter/metadata/BBBC021_v1_moa.csv       \
   --images /data1/peter/segmented                         \
