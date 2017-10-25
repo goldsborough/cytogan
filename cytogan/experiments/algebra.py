d146d90d7e5e752a68e73fefb5cb65c4b084140a
goldsborough <peter@goldsborough.me>
2017-10-25 19:01:08 -0400

    Working on image algebra

--- cytogan/experiments/algebra.py
+++ cytogan/experiments/algebra.py
@@ -69,7 +69,8 @@ class MoaCanceling(Experiment):
             result_vectors, treatment_profiles['profile'])
         moas = np.array(treatment_profiles['moa'].iloc[nearest_neighbors])
 
-        target_moa = treatment_profiles[self.compound]['moa']
+        predicate = treatment_profiles['compound'] == self.compound
+        target_moa = treatment_profiles[predicate].iloc[0]['moa']
         print(target_moa)
         accuracy = np.mean(moas == target_moa)
         log.info('Accuracy for MOA canceling experiment: %.3f', accuracy)
