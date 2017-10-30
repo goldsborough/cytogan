69d3cf476c2f13d95c737446c1fff547e170da55
goldsborough <peter@goldsborough.me>
2017-10-30 16:08:35 -0400

    Modified prune script

--- scripts/prune_runs.py
+++ scripts/prune_runs.py
@@ -4,14 +4,21 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument('runs-path')
+parser.add_argument('runs')
 parser.add_argument('--dry', action='store_true')
 options = parser.parse_args()
 
-for run in os.listdir(options.runs_path):
-    contents = os.listdir(run)
-    if not ('checkpoints' in contents or 'summaries' in contents):
-        if options.dry:
-            print('rm -r {0}'.format(run))
-        else:
-            os.rmdir(run)
+print(options)
+
+assert os.path.exists(options.runs), 'Run path does not exist!'
+
+for run in os.listdir(options.runs):
+    run_path = os.path.join(options.runs, run)
+    contents = os.listdir(run_path)
+
+    if 'checkpoints' in contents or 'summaries' in contents:
+        print('Keeping {0}'.format(run_path))
+    else:
+        print('rm -r {0}'.format(run_path))
+        if not options.dry:
+            os.rmdir(run_path)
